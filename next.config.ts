2c2e163f1708147ab2989bf4944c9763eaf5a176
young <gustmdehdud@gmail.com>
2026-04-09 03:22:16 +0900

    next config 이미지 경로  추가

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   sassOptions: {
     additionalData: `@use "@/styles/color_variables" as *;`,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
