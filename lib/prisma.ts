39bbaf1ee23a2f8a0a4f5308ef5568bc664cbf3c
young <gustmdehdud@gmail.com>
2026-04-14 04:23:48 +0900

    prisma, axios 셋팅 추가

---
+++ lib/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@/app/generated/prisma";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { env } from "prisma/config";
+
+const adapter = new PrismaMariaDb({
+  host: env("DATABASE_HOST"),
+  user: env("DATABASE_USER"),
+  password: env("DATABASE_PASSWORD"),
+  database: env("DATABASE_NAME"),
+  port: Number(env("DATABASE_PORT")),
+  connectionLimit: 5,
+});
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
