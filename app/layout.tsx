17424487e63aa4c471d29bd777ceedccc9cfdc37
young <gustmdehdud@gmail.com>
2026-04-14 17:58:47 +0900

    기본 스타일 설정 변경 및 폰트 설정 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,19 @@ import type { Metadata } from "next";
 import Header from "@/components/Common/Header";
 import Footer from "@/components/Common/Footer";
 import Container from "@/components/Common/Container";
-import "./globals.css";
+import "@/styles/global.scss";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "My House",
   description: "",
 };
+
+const pretendard = localFont({
+  src: "../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  variable: "--font-pretendard",
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.variable}>
       <body>
         <Header />
         <Container>{children}</Container>
