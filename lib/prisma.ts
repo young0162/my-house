63e23a6e9c56fadd245a735cc569cb16fbce1113
young <gustmdehdud@gmail.com>
2026-06-02 23:47:29 +0900

    prisma 6 , next-auth 5 으로 버전 변경

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from "@/app/generated/prisma";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { env } from "prisma/config";
 
 const adapter = new PrismaMariaDb({
-  host: env("DATABASE_HOST"),
-  user: env("DATABASE_USER"),
-  password: env("DATABASE_PASSWORD"),
-  database: env("DATABASE_NAME"),
-  port: Number(env("DATABASE_PORT")),
+  host: process.env.DATABASE_HOST!,
+  user: process.env.DATABASE_USER!,
+  password: process.env.DATABASE_PASSWORD!,
+  database: process.env.DATABASE_NAME!,
+  port: Number(process.env.DATABASE_PORT),
   connectionLimit: 5,
 });
 
