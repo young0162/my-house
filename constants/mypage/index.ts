663c2cdf602d7ab9dc2720ee36a456e9eb1f1673
young <gustmdehdud@gmail.com>
2026-06-04 12:33:04 +0900

    마이페이지-프로필 상수값, 타입 추가

--- constants/mypage/index.ts
+++ constants/mypage/index.ts
@@ -35,3 +35,64 @@ export const ORDER_STATUS_OPTIONS = [
   "배송완료",
   "구매확정",
 ] as const;
+
+export const PROFILE_ACTIVITY_TABS = [
+  "모두보기",
+  "사진",
+  "집들이",
+  "노하우",
+  "스크랩북",
+  "좋아요",
+] as const;
+
+export const PROFILE_STATS = [
+  { id: "scrapbook", label: "스크랩북" },
+  { id: "likes", label: "좋아요" },
+  { id: "coupons", label: "내 쿠폰" },
+] as const;
+
+export const MOCK_PROFILE = {
+  userId: "user-01",
+  nickname: "나의집",
+  followerCount: 12,
+  followingCount: 34,
+  scrapbookCount: 5,
+  likeCount: 28,
+  couponCount: 0,
+};
+
+export const MOCK_LIKED_CONTENTS = [
+  {
+    id: "1",
+    title: "모던 북유럽 거실 인테리어",
+    type: "집들이",
+    thumbnailUrl: "https://picsum.photos/256/256?random=10",
+    href: "/contents/1",
+  },
+  {
+    id: "2",
+    title: "작은 방 수납 노하우 10가지",
+    type: "노하우",
+    thumbnailUrl: "https://picsum.photos/256/256?random=11",
+  },
+  {
+    id: "3",
+    title: "화이트 원목 침실 꾸미기",
+    type: "집들이",
+    thumbnailUrl: "https://picsum.photos/256/256?random=12",
+    href: "/contents/3",
+  },
+  {
+    id: "4",
+    title: "조명으로 분위기 바꾸기",
+    type: "사진",
+    thumbnailUrl: "https://picsum.photos/256/256?random=13",
+  },
+  {
+    id: "5",
+    title: "주방 정리 꿀팁",
+    type: "노하우",
+    thumbnailUrl: "https://picsum.photos/256/256?random=14",
+    href: "/contents/5",
+  },
+];
--- types/mypage/index.ts
+++ types/mypage/index.ts
@@ -1,9 +1,29 @@
-import { SUB_TABS, ORDER_STEPS, PERIOD_OPTIONS, ORDER_STATUS_OPTIONS } from "@/constants/mypage";
+import { SUB_TABS, ORDER_STEPS, PERIOD_OPTIONS, ORDER_STATUS_OPTIONS, PROFILE_ACTIVITY_TABS } from "@/constants/mypage";
 
 export type SubTab = (typeof SUB_TABS)[number];
 export type OrderStep = (typeof ORDER_STEPS)[number];
 export type PeriodOption = (typeof PERIOD_OPTIONS)[number];
 export type OrderStatusOption = (typeof ORDER_STATUS_OPTIONS)[number];
+export type ProfileActivityTab = (typeof PROFILE_ACTIVITY_TABS)[number];
+
+export interface ProfileSummary {
+  userId: string;
+  nickname: string;
+  profileImageUrl?: string;
+  followerCount: number;
+  followingCount: number;
+  scrapbookCount: number;
+  likeCount: number;
+  couponCount: number;
+}
+
+export interface LikedContent {
+  id: string;
+  title: string;
+  type: string;
+  thumbnailUrl: string;
+  href?: string;
+}
 
 export interface OrderProduct {
   name: string;
